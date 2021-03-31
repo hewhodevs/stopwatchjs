71d205e06b86d5da6e71356b2ea4ec8b352486f4
hewhodevs <hewhodevs@outlook.com>
2021-03-31 22:47:02 +1030

    added button toggle logic

--- stopwatch.js
+++ stopwatch.js
@@ -3,8 +3,10 @@ document.body.onload = addElementsOnLoad;
 const stopwatchContainer = document.getElementById("stopwatchContainer");
 const startButton = createStopwatchButton("startButton", "Start", true);
 const stopButton = createStopwatchButton("stopButton", "Stop", true);
-const resumeButton = createStopwatchButton("resumeButton", "Resume", true);
 const resetButton = createStopwatchButton("resetButton", "Reset", false);
+startButton.onclick = onStartButtonClick;
+stopButton.onclick = onStopButtonClick;
+resetButton.onclick = onResetButtonClick;
 
 function createStopwatchButton(id, buttonText, isEnabled) {
   let button = document.createElement("BUTTON");
@@ -18,4 +20,31 @@ function createStopwatchButton(id, buttonText, isEnabled) {
 function addElementsOnLoad() {
   stopwatchContainer.appendChild(startButton);
   stopwatchContainer.appendChild(resetButton);
+}
+
+function onStartButtonClick() {
+  startButton.remove();
+  showStopButton();
+  resetButton.disabled = false;
+}
+
+function onStopButtonClick() {
+  stopButton.remove();
+  showStartButtton();
+}
+
+function onResetButtonClick() {
+  resetButton.disabled = true;
+  if(document.getElementById("stopButton")) {
+    stopButton.remove();
+    showStartButtton();
+  }
+}
+
+function showStartButtton() {
+  stopwatchContainer.insertBefore(startButton, resetButton);
+}
+
+function showStopButton() {
+  stopwatchContainer.insertBefore(stopButton, resetButton);
 }
